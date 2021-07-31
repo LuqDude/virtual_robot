7f7f896616080256b1b81c01edd9ec68cad9aa74
LuqDude <luqmanforge@gmail.com>
2021-07-31 16:35:45 -0400

    Added SampleOpMode.java so the sim can actually run

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/SampleOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.navigation.*;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Sample OpMode - DO NOT RUN")
+public class SampleOpMode extends OpMode {
+
+
+
+    public void init(){
+
+    }
+
+    public void init_loop(){
+
+    }
+
+    public void loop(){
+
+
+    }
+
+}
